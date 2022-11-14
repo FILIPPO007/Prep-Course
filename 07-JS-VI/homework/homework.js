7d7413dfc27a981b1daf5d9f44fb9efe824417cc
FILIPPO007 <juanfelipe107@gmail.com>
2022-11-14 18:30:26 -0500

    homework JS-VI OK

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,35 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  let upperVocal = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+  let lowerVocal = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    
+    let letters = nombre.split('')
+
+    for(let i = 0; i < lowerVocal.length; i++){
+        if(letters[0] === lowerVocal[i] ){
+            letters[0] = upperVocal[i]
+            break;
+        }
+    }
+     letters = letters.join('')
+    return letters
+
+
+    
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  cb()
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  return cb(n1,n2)
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +40,17 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  let sumTotal = numeros.reduce((previous, current) => previous + current, 0) 
+  cb(sumTotal)
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(function(element){
+    cb(element)
+  })
 }
 
 function map(array, cb) {
@@ -35,12 +58,16 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  const nums = array.map(x=>cb(x));
+  return nums;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  const items = array.filter(x => x[0] === 'a')
+  return items
 }
 
 // No modificar nada debajo de esta línea
